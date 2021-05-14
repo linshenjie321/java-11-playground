fea49b0c10b41c94b7a72d243b15ee4a9667bee2
Shenjie Lin <linshenjie321@gmail.com>
2021-05-14 18:07:09 -0400

    initial commit

---
+++ src/com/sjl/learning/java11/newstringmethods/NewStringMethodTester.java
@@ -0,0 +1,10 @@
+package com.sjl.learning.java11.newstringmethods;
+
+public class NewStringMethodTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
